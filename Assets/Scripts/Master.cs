2f02f8e9c1ad84ea92575c69f28e35eec3d6218e
MalaiaGarnet <jimjack@naver.com>
2020-08-14 11:57:36 +0900

    마스터 오브젝트 추가

---
+++ Assets/Scripts/Master.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Master : MonoBehaviour
+{
+    public static Master Instance;
+
+    private void Awake()
+    {
+        if (Instance != null)
+            Destroy(gameObject);
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
