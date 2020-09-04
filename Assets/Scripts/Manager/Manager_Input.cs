da359fb791eec9eba0a227e90433e5d2ba5d953f
Kim Ju Hyeon <wallon1473@naver.com>
2020-09-05 01:14:27 +0900

    스크립트 폴더 정돈 및 싱글톤 개선
    스크립트 폴더 정돈 및 싱글톤 개선!

--- Assets/Scripts/Manager/Manager_Input.cs
+++ Assets/Scripts/Manager/Manager_Input.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
 
 public struct User_Input
 {
@@ -49,9 +50,52 @@ public struct User_Input
     }
 };
 
+public struct SInteractAction
+{
+    public SlowTapInteraction  SlowTap;
+    public TapInteraction      Tap;
+    public PressInteraction    Press;
+    public MultiTapInteraction MultiTap;
+    public HoldInteraction     Hold;
+
+    public IInputInteraction inputInteraction;
+    public void SetInteractAction(ref object _Objects)
+    {
+    }
+}
+
 public class Manager_Input : SingleToneMonoBehaviour<Manager_Input>
 {
-    public User_Input m_Player_Input;
+    private PlayerInputAction m_InputAction;
+    public  User_Input        m_Player_Input;
+    private SInteractAction   m_InteractAction;
+    
+    void Awake()
+    {
+        m_InputAction = new PlayerInputAction();
+        m_InputAction.PlayerMoves.Move.performed += context => InputAct_Moving(context); // 이동 전달
+    }
+
+    public void InputAct_Moving(InputAction.CallbackContext _Context)
+    {
+        
+        m_Player_Input.Move_X += _Context.ReadValue<Vector2>().x;
+        m_Player_Input.Move_Y += _Context.ReadValue<Vector2>().y;
+
+        if(_Context.interaction is PressInteraction)//만약 누르고있을때
+        {
+            m_InteractAction.Press.behavior = PressBehavior.PressOnly;
+            if(m_InteractAction.Press.behavior == PressBehavior.PressOnly)
+            {
+ 
+            }
+        }
+    }
+
+    public bool GetButtonDown()
+    {
+        return false;
+    }
 
     // Start is called before the first frame update
     void Start()
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+class PlayerController : MonoBehaviour
+{
+    Manager_Input m_inputs;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
