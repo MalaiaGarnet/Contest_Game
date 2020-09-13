b03342ae3ea60526218e6bb831e42e8541f34a33
MalaiaGarnet <jimjack@naver.com>
2020-09-13 23:51:31 +0900

    맵 업데이트, 조작 개선 (2차)

---
+++ Assets/Scripts/DebugLogger.cs
@@ -0,0 +1,44 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugLogger : SingleToneMonoBehaviour<DebugLogger>
+{
+    public GameObject m_Contents;
+    public Text m_DebugText;
+    public Queue<string> texts = new Queue<string>();
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        /*
+        if (Input.GetKeyDown(KeyCode.F1))
+            m_Contents.SetActive(!m_Contents.activeSelf);
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            texts = new Queue<string>();
+            m_DebugText.text = "";
+        }
+        */
+    }
+
+    public void AddText(string _text)
+    {
+        texts.Enqueue(_text);
+        if (texts.Count > 30)
+            texts.Dequeue();
+
+        Queue<string> temp_texts = new Queue<string>(texts);
+
+        m_DebugText.text = "";
+        while (temp_texts.Count > 0)
+            m_DebugText.text += temp_texts.Dequeue() + "\n";
+
+    }
+}
