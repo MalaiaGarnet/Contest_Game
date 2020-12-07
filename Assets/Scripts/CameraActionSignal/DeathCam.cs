f809ebc9fbf22e71c03a52d8b3def926fd36e0ca
Kim Ju Hyeon <wallon1473@naver.com>
2020-12-07 13:58:46 +0900

    데스캠 잘못된 구조 픽스중 & 클로킹 함수 수정 & 스턴건 버그 수정 등등
    데스캠 잘못된 구조 픽스중 & 클로킹 함수 수정 & 스턴건 버그 수정 등등

--- Assets/Scripts/CameraActionSignal/DeathCam.cs
+++ Assets/Scripts/CameraActionSignal/DeathCam.cs
@@ -1,18 +1,21 @@
 ﻿using Cinemachine;
-using Devcat;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Playables;
+using Greyzone.GUI;
 
 /// <summary>
 /// 데스 카메라 컨트롤 클래스입니다.
 /// 
 /// </summary>
-public class DeathCam : MonoBehaviour
+public class DeathCam : SingleToneComponent<DeathCam>
 {
     public PlayableDirector PlayableDirector;
     public CinemachineVirtualCamera[] VCams = new CinemachineVirtualCamera[4];
-    public GameObject Guaurd;
+
+    public Transform Killer { get; protected set; }
+    public Transform DeadPlayer { get; protected set; }
+
     public CinemachineBrain DeathCamBrain;
 
     enum VCamIndex
@@ -23,39 +26,64 @@ public class DeathCam : MonoBehaviour
         RIGHT
     }
 
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
     void Start()
     {
-        // 1인칭 카메라랑 일단 분리
-        DeathCamBrain.enabled = false;
+        gameObject.SetActive(false);
+        PlayableDirector.gameObject.SetActive(false);
+        ActiveDeathCamVirtualCameras();
     }
 
-    public void EnumValueChange()
+    public void FindSetDeathPlayer(Transform _Ragdoll)
     {
+        BindTarget(0, Killer, _Ragdoll);    //Back
+        BindTarget(1, _Ragdoll, Killer);    //Front
+        BindTarget(2, Killer, _Ragdoll);    //Left
+        BindTarget(3, _Ragdoll, Killer);    //Right
 
+        DeadPlayer = _Ragdoll;
     }
+
+    public void FindSetKillPlayer(Transform _Killer)
+    {
+        Killer = _Killer;
+    }
+
+    void BindTarget(byte _CamIndex, Transform _LookAt, Transform _Follow)
+    {
+        VCams[_CamIndex].LookAt = _LookAt;
+        VCams[_CamIndex].Follow = _Follow;
+    }
+
+    void ActiveDeathCamVirtualCameras()
+    {
+        for (byte index = 0; index < VCams.Length; index++)
+        {
+            VCams[index].VirtualCameraGameObject.SetActive(true);
+        }
+    }
+
     public void StartCam()
     {
-        int back = 0;
-        var value = new EnumDictionary<VCamIndex, int>()
-        {
-            { VCamIndex.BACK, 0},
-            { VCamIndex.FRONT, 1 },
-            { VCamIndex.LEFT, 2 },
-            { VCamIndex.RIGHT, 3 }
-        };
-
         // 켜지면 이 카메라가 발격 (더 좋은 방법 떠오르기전 까진 일단 이렇게...)
-        DeathCamBrain.enabled = true;
-        VCams[0].LookAt = Guaurd.transform;
-        VCams[3].Follow = Guaurd.transform;
+        DeathCamBrain.gameObject.SetActive(true);
+        PlayableDirector.gameObject.SetActive(true);
         PlayableDirector.Play();
+        TooltipManager.Instance.tooltip_HeadMessage.ShowMessage(MessageStyle.ON_HEAD_MSG, "NO SIGNAL...", DeadPlayer.localPosition);
         StartCoroutine(EndCam());
     }
 
     IEnumerator EndCam()
     {
         yield return new WaitForSeconds((float)PlayableDirector.duration);
-        DeathCamBrain.enabled = false;
+        DeathCamBrain.gameObject.SetActive(false);
+        PlayableDirector.gameObject.SetActive(false);
+        PlayableDirector.Stop();
+        TooltipManager.Instance.tooltip_HeadMessage.HideMessage();
         yield return null;
     }
 }
